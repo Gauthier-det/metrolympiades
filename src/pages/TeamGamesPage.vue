2bff4cd93da37c4a1f43ebadf152c81e57536843
Noé <noebrault@live.fr>
2025-04-26 20:41:32 +0200

    Adding a page with the list of all matches for a team (non-functional)

---
+++ src/pages/TeamGamesPage.vue
@@ -0,0 +1,108 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import Sidebar from "../components/Sidebar.vue";
+
+import "../assets/games.css";
+
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const teamId = ref(route.params.id);
+
+const team = ref(null);
+
+const user = JSON.parse(localStorage.getItem("user"));
+const errorMessage = ref("");
+
+const matchs = ref([]);
+
+
+async function fetchMatchs() {
+  try {
+    const response = await fetch(`http://localhost:3000/matches/${teamId.value}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Impossible de récupérer les matchs");
+    }
+    const data = await response.json();
+    matchs.value = data;
+  } catch (error) {
+    errorMessage.value = "Erreur lors de la récupération des matchs ";
+
+  }
+}
+
+async function fetchTeamInfo() {
+  try {
+    const response = await fetch(`http://localhost:3000/teams/${teamId.value}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Impossible de récupérer l'équipe");
+    }
+    const data = await response.json();
+    team.value = data;
+    console.log(team.value);
+  } catch (error) {
+    errorMessage.value = "Erreur lors de la récupération de l'équipe";
+  }
+}
+
+
+function formatDate(dateString) {
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return new Date(dateString).toLocaleDateString("fr-FR", options);
+}
+
+onMounted(() => {
+  fetchTeamInfo();
+  fetchMatchs();
+});
+</script>
+
+<template>
+  <div class="container">
+    <Sidebar :user="user" />
+
+    <div class="main-content">
+      <div class="header">
+        <h1>Matchs de ${{team}}</h1>
+      </div>
+      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+
+      <div class="matches-container">
+        <div v-for="match in matchs" :key="match.id" class="match-card">
+          <div class="match-header">
+            <span class="match-type">{{ match.activity }}</span>
+            <span class="match-time">{{ formatDate(match.startedAt) }}</span>
+          </div>
+          <div class="match-teams">
+            <div class="team">
+              <span class="team-name">{{ match.team1 }}</span>
+              <span class="team-score">{{ match.team1Score }}</span>
+            </div>
+            <div class="team">
+              <span class="team-name">{{ match.team2 }}</span>
+              <span class="team-score">{{ match.team2Score }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
